fb78cfe66a540ec7984c788d8e6281bfecbbad6b
Robert Alm Nilsson <rorialni@gmail.com>
2017-07-14 13:51:11 +0200

    Reads stdin and prints a prompt

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,31 @@
+use std::io::Write;
+
 fn main() {
+    let mut line = String::new();
+    loop {
+        line.clear();
+        print!("twitch> ");
+        std::io::stdout().flush().unwrap();
+        let chars_read = std::io::stdin().read_line(&mut line).unwrap();
+        if chars_read == 0 {
+            println!();
+            break;
+        }
+        if line == "\n" {
+        } else if line == "status\n" {
+            
+        } else {
+            println!("{}", line);
+        }
+    }
+}
+
+struct Api {
+
+}
+
+impl Api {
+    fn get() {
+
+    }
 }
