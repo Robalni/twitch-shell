ff9162ae1ae29c85363c31815526df0d74eb0679
Robert Alm Nilsson <rorialni@gmail.com>
2017-07-14 14:26:24 +0200

    Use client id header

--- src/api.rs
+++ src/api.rs
@@ -1,7 +1,8 @@
 extern crate curl;
-use self::curl::easy::Easy;
+use self::curl::easy::{Easy, List};
 
 const BASE_URL: &str = "https://api.twitch.tv/kraken/";
+const CLIENT_ID: &str = "mpw1gvfyzd1mtnm7rzoln5icmkdecys";
 
 pub struct Api {
     easy: Easy,
@@ -16,6 +17,9 @@ impl Api {
 
     pub fn get(&mut self, path: &str) {
         self.easy.url(&(BASE_URL.to_owned() + path)).unwrap();
+        let mut headers = List::new();
+        headers.append(&("Client-ID: ".to_owned() + CLIENT_ID)).unwrap();
+        self.easy.http_headers(headers).unwrap();
         let mut buf = Vec::new();
         {
             let mut transfer = self.easy.transfer();
