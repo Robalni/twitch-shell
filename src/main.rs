ac1e24caf5d1472323201da5355c04375bee4ca4
Robert Alm Nilsson <rorialni@gmail.com>
2017-07-15 00:00:42 +0200

    Search command

--- src/main.rs
+++ src/main.rs
@@ -35,18 +35,43 @@ fn execute_command(cmd: parser::Command, api: &mut Api, username: &str)
         Command::Empty => {},
         Command::Simple(c) => {
             match c[0] {
+                "exit" => {
+                    std::process::exit(0);
+                },
+                "search" => {
+                    let limit = 7;
+                    let offset = if c.len() > 2 {
+                        (c[2].parse::<i32>().unwrap() - 1) * limit
+                    } else {
+                        0
+                    };
+                    let path = format!("search/streams?q={}&offset={}&limit={}",
+                                       encode(c[1]), offset, limit);
+                    let obj = api.get(&(path));
+                    let o = match obj {
+                        Ok(v) => v,
+                        Err(e) => { return Err(e); },
+                    };
+                    let mut i = 0;
+                    let ref list = o["streams"];
+                    while !list[i].is_null() {
+                        let ref l = list[i];
+                        println!("{} playing {}\n  {}",
+                                 l["channel"]["display_name"],
+                                 l["game"],
+                                 l["channel"]["status"]);
+                        i += 1;
+                    }
+                },
                 "status" => {
                     let obj = api.get(&("channels/".to_owned()
                                         + &encode(username)));
                     let o = match obj {
                         Ok(v) => v,
-                        Err(e) => { return Err(e); }
+                        Err(e) => { return Err(e); },
                     };
                     println!("{} playing {}\n  {}",
                              o["display_name"], o["game"], o["status"]);
-                },
-                "exit" => {
-                    std::process::exit(0);
                 },
                 _ => {
                     return Err("Unknown command: ".to_owned() + c[0]);
