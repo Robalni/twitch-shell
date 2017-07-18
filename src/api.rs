0484b32ef1c2291e8052521d34d5c5111b7b8b0f
Robert Alm Nilsson <rorialni@gmail.com>
2017-07-18 12:22:15 +0200

    login works

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,15 @@
 extern crate curl;
 extern crate json;
+extern crate rand;
 use self::curl::easy::{Easy, List};
+use std::io::Read;
+use self::json::JsonValue;
+use self::rand::Rng;
 
 const BASE_URL: &str = "https://api.twitch.tv/kraken/";
-const CLIENT_ID: &str = "mpw1gvfyzd1mtnm7rzoln5icmkdecys";
+const CLIENT_ID: &str = "dl1xe55lg2y26u8njj769lxhq3i47r";
+const CLIENT_SECRET: &str = "39pxzfwymvpmaqoovj6fyj41idov1j";
+const REDIRECT_URI: &str = "http://localhost:49814";
 
 pub struct Api {
     easy: Easy,
@@ -11,13 +17,15 @@ pub struct Api {
 
 impl Api {
     pub fn new() -> Self {
+        curl::init();
         Api {
             easy: Easy::new(),
         }
     }
 
-    pub fn get(&mut self, path: &str) -> Result<json::JsonValue, String> {
+    pub fn get(&mut self, path: &str) -> Result<JsonValue, String> {
         self.easy.url(&(BASE_URL.to_owned() + path)).unwrap();
+        self.easy.put(false).unwrap();
         let mut headers = List::new();
         headers.append(&("Client-ID: ".to_owned() + CLIENT_ID)).unwrap();
         self.easy.http_headers(headers).unwrap();
@@ -31,6 +39,122 @@ impl Api {
             transfer.perform().unwrap();
         }
         let json_str = String::from_utf8(buf).unwrap();
+        let obj = json::parse(&json_str);
+        match obj {
+            Ok(o) => {
+                if o["error"].is_null() {
+                    Ok(o)
+                } else {
+                    Err(o["error"].to_string()
+                        + " (" + &(o["message"].to_string()) + ")")
+                }
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
+
+    pub fn put(&mut self, path: &str, mut data: &[u8], oauth: &str)
+               -> Result<JsonValue, String> {
+        self.easy.url(&(BASE_URL.to_owned() + path)).unwrap();
+        let mut headers = List::new();
+        headers.append(&("Client-ID: ".to_owned() + CLIENT_ID)).unwrap();
+        headers.append(&("Authorization: OAuth ".to_owned() + oauth)).unwrap();
+        self.easy.http_headers(headers).unwrap();
+        self.easy.put(true).unwrap();
+        self.easy.post_field_size(data.len() as u64).unwrap();
+        let mut response = Vec::new();
+        {
+            let mut transfer = self.easy.transfer();
+            transfer.read_function(|buf| {
+                Ok(data.read(buf).unwrap_or(0))
+            }).unwrap();
+            transfer.write_function(|buf| {
+                response.extend_from_slice(buf);
+                Ok(buf.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+        let json_str = String::from_utf8(response).unwrap();
+        let obj = json::parse(&json_str);
+        match obj {
+            Ok(o) => {
+                if o["error"].is_null() {
+                    Ok(o)
+                } else {
+                    Err(o["error"].to_string()
+                        + " (" + &(o["message"].to_string()) + ")")
+                }
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
+
+    pub fn post(&mut self, path: &str, mut data: &[u8], oauth: &str)
+                -> Result<JsonValue, String> {
+        self.easy.url(&(BASE_URL.to_owned() + path)).unwrap();
+        let mut headers = List::new();
+        headers.append(&("Client-ID: ".to_owned() + CLIENT_ID)).unwrap();
+        headers.append(&("Authorization: OAuth ".to_owned() + oauth)).unwrap();
+        self.easy.http_headers(headers).unwrap();
+        self.easy.post(true).unwrap();
+        self.easy.post_field_size(data.len() as u64).unwrap();
+        let mut response = Vec::new();
+        {
+            let mut transfer = self.easy.transfer();
+            transfer.read_function(|buf| {
+                Ok(data.read(buf).unwrap_or(0))
+            }).unwrap();
+            transfer.write_function(|buf| {
+                response.extend_from_slice(buf);
+                Ok(buf.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+        let json_str = String::from_utf8(response).unwrap();
+        let obj = json::parse(&json_str);
+        match obj {
+            Ok(o) => {
+                if o["error"].is_null() {
+                    Ok(o)
+                } else {
+                    Err(o["error"].to_string()
+                        + " (" + &(o["message"].to_string()) + ")")
+                }
+            },
+            Err(e) => Err(e.to_string()),
+        }
+    }
+
+    pub fn get_login_url(&mut self, state: &str) -> String {
+        let state_url = self.easy.url_encode(state.as_bytes());
+        format!("{}oauth2/authorize?client_id={}&redirect_uri=http://localhost:49814&response_type=code&scope=channel_editor&state={}",
+                BASE_URL, CLIENT_ID, state_url)
+    }
+
+    pub fn login(&mut self, code: &str) -> Result<JsonValue, String> {
+        let mut osrng = rand::os::OsRng::new().unwrap();
+        let state: String = osrng.gen_ascii_chars().take(20).collect();
+        let url = format!("{}oauth2/token?client_id={}&client_secret={}&code={}\
+                           &grant_type=authorization_code\
+                           &redirect_uri={}&state={}",
+                          BASE_URL, CLIENT_ID, CLIENT_SECRET, code,
+                          REDIRECT_URI, state);
+        self.easy.url(&url).unwrap();
+        self.easy.post(true).unwrap();
+        let mut data: &[u8] = &[0];
+        let mut response = Vec::new();
+        {
+            let mut transfer = self.easy.transfer();
+            transfer.read_function(|buf| {
+                Ok(data.read(buf).unwrap_or(0))
+            }).unwrap();
+            transfer.write_function(|buf| {
+                response.extend_from_slice(buf);
+                Ok(buf.len())
+            }).unwrap();
+            transfer.perform().unwrap();
+        }
+        let json_str = String::from_utf8(response).unwrap();
         let obj = json::parse(&json_str);
         match obj {
             Ok(o) => {
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,58 @@
-extern crate urlencoding;
+extern crate urlparse;
 extern crate yansi;
+extern crate tiny_http;
+extern crate rand;
 
 mod api;
 mod parser;
 
+use rand::Rng;
+use tiny_http::{Server, Response};
 use yansi::Paint;
 use parser::Command;
-use urlencoding::encode;
+use urlparse::{urlparse, GetQuery, quote};
 use api::Api;
-use std::io::Write;
+use std::io::{Write, Read};
+use std::fs::File;
+use std::error::Error;
 
 fn main() {
-    let username = "robalni";
+    let username = &match string_from_file("username") {
+        Ok(v) => v,
+        Err(e) => { println!("{}", e); return; },
+    };
+    let oauth = &match string_from_file(&(format!(".{}.oauth", username))) {
+        Ok(v) => v,
+        Err(e) => { println!("{}", e); String::new() },
+    };
     let mut line = String::new();
     let mut api = Api::new();
     loop {
         line.clear();
-        print!("{} ", Paint::purple("twitch>").bold());
-        std::io::stdout().flush().unwrap();
+        show_prompt(username);
         let chars_read = std::io::stdin().read_line(&mut line).unwrap();
         if chars_read == 0 {
             println!();
             break;
         }
         let cmd = parser::parse(&line);
-        match execute_command(cmd, &mut api, username) {
+        match execute_command(cmd, &mut api, username, oauth) {
             Err(e) => println!("{}", Paint::red(format!("Error: {}", e))),
             Ok(_) => {},
         }
     }
 }
 
-fn execute_command(cmd: parser::Command, api: &mut Api, username: &str)
+fn show_prompt(username: &str) {
+    print!("{} ", Paint::purple(format!("{}@twitch>", username)).bold());
+    std::io::stdout().flush().unwrap();
+}
+
+fn execute_command(cmd: parser::Command, api: &mut Api,
+                   username: &str, oauth: &str)
                    -> Result<(), String> {
     match cmd {
-        Command::Empty => {},
+        Command::Empty => { Ok(()) },
         Command::Simple(c) => {
             match c[0] {
                 "exit" => {
@@ -42,6 +60,10 @@ fn execute_command(cmd: parser::Command, api: &mut Api, username: &str)
                 },
                 "help" => {
                     print_help();
+                    Ok(())
+                },
+                "login" => {
+                    login(api, username)
                 },
                 "search" => {
                     let limit = 7;
@@ -50,8 +72,12 @@ fn execute_command(cmd: parser::Command, api: &mut Api, username: &str)
                     } else {
                         0
                     };
+                    let q = match quote(c[1], b"") {
+                        Ok(v) => v,
+                        Err(e) => return Err(e.to_string()),
+                    };
                     let path = format!("search/streams?q={}&offset={}&limit={}",
-                                       encode(c[1]), offset, limit);
+                                       q, offset, limit);
                     let obj = api.get(&(path));
                     let o = match obj {
                         Ok(v) => v,
@@ -67,16 +93,22 @@ fn execute_command(cmd: parser::Command, api: &mut Api, username: &str)
                                  l["channel"]["status"]);
                         i += 1;
                     }
+                    Ok(())
                 },
                 "status" => {
+                    let uname_url = match quote(username, b"") {
+                        Ok(v) => v,
+                        Err(e) => return Err(e.to_string()),
+                    };
                     let obj = api.get(&("channels/".to_owned()
-                                        + &encode(username)));
+                                        + &uname_url));
                     let o = match obj {
                         Ok(v) => v,
-                        Err(e) => { return Err(e); },
+                        Err(e) => return Err(e),
                     };
                     println!("{} playing {}\n  {}",
                              o["display_name"], o["game"], o["status"]);
+                    Ok(())
                 },
                 _ => {
                     return Err("Unknown command: ".to_owned() + c[0]);
@@ -84,15 +116,119 @@ fn execute_command(cmd: parser::Command, api: &mut Api, username: &str)
             }
         },
         Command::Assign(lhs, rhs) => {
+            let joined_rhs = rhs.join(" ");
+            match lhs[0] {
+                "status" => {
+                    let uname_url = match quote(username, b"") {
+                        Ok(v) => v,
+                        Err(e) => return Err(e.to_string()),
+                    };
+                    let path = format!("channels/{}", uname_url);
+                    let data = format!("channel[status]={}", joined_rhs);
+                    let s = api.put(&path, data.as_bytes(), oauth);
+                    match s {
+                        Ok(_) => {},
+                        Err(e) => return Err(e),
+                    }
+                    println!("set {} to {}", lhs[0], joined_rhs);
+                    Ok(())
+                },
+                _ => {
+                    return Err("Unknown variable: ".to_owned() + lhs[0]);
+                }
+            }
         },
     }
+}
+
+fn string_from_file(filename: &str) -> Result<String, String> {
+    let path = std::env::home_dir().unwrap().join(".twitch").join(filename);
+    match File::open(path) {
+        Ok(mut f) => {
+            let mut buf = String::new();
+            match f.read_to_string(&mut buf) {
+                Ok(_) => Ok(buf.trim().to_owned()),
+                Err(e) => Err(e.description().to_owned()),
+            }
+        },
+        Err(e) => Err(e.description().to_owned()),
+    }
+}
+
+fn string_to_file(filename: &str, string: &str) -> Result<(), String> {
+    let path = std::env::home_dir().unwrap().join(".twitch").join(filename);
+    match File::create(path) {
+        Ok(mut f) => {
+            let buf = string.as_bytes();
+            match f.write_all(&buf) {
+                Ok(_) => {
+                    match f.write_all(b"\n") {
+                        Ok(_) => Ok(()),
+                        Err(e) => Err(e.description().to_owned()),
+                    }
+                },
+                Err(e) => Err(e.description().to_owned()),
+            }
+        },
+        Err(e) => Err(e.description().to_owned()),
+    }
+}
+
+fn login(api: &mut Api, username: &str) -> Result<(), String> {
+    let server = Server::http("127.0.0.1:49814").unwrap();
+    let mut osrng = rand::os::OsRng::new().unwrap();
+    let state: String = osrng.gen_ascii_chars().take(20).collect();
+    println!("{}\n", api.get_login_url(&state));
+    println!("Open the url above in a web browser and authorize with Twitch.");
+    println!("Come back to this shell when you are done.");
+    let rq = match server.recv() {
+        Ok(rq) => rq,
+        Err(e) => return Err(e.to_string()),
+    };
+    let url = urlparse(rq.url());
+    let query = match url.get_parsed_query() {
+        Some(v) => v,
+        None => return Err("Could not get code from Twitch \
+                            (failed to parse url)".to_owned()),
+    };
+    let code = match query.get_first_from_str("code") {
+        Some(v) => v,
+        None => return Err("Could not get code from Twitch \
+                            (failed to find the code in the url)"
+                           .to_owned()),
+    };
+    let got_state = match query.get_first_from_str("state") {
+        Some(v) => v,
+        None => return Err("Could not get state from Twitch \
+                            (failed to find the state in the url)"
+                           .to_owned()),
+    };
+    if got_state != state {
+        return Err("Got an invalid response (the state is different)"
+                   .to_owned());
+    }
+    let obj = api.login(&code);
+    let o = match obj {
+        Ok(v) => v,
+        Err(e) => return Err(e),
+    };
+    let ref oauth = o["access_token"];
+    match string_to_file(&(format!(".{}.oauth", username)), &oauth.to_string()) {
+        Err(e) => return Err(e),
+        _ => (),
+    }
+    let r = "Everything went well. Now go back to the shell.";
+    rq.respond(Response::from_string(r)).unwrap();
+    println!("Done!");
     Ok(())
 }
 
 fn print_help() {
-    println!("Available commands:");
-    println!("  exit                  Exits the shell");
-    println!("  help                  Prints help text");
-    println!("  search <str> [page]   Searches for streams");
-    println!("  status                Prints information about your channel");
+    println!("Available commands:
+  exit                  Exits the shell
+  help                  Prints help text
+  login                 Logs in to Twitch
+  search <str> [page]   Searches for streams
+  status                Prints information about your channel
+");
 }
